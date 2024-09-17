4d1041354bf38eb4a6c9d5d07ae698e9bb2a512a
User <grisha.sirencko@gmail.com>
2024-09-17 18:50:08 +0300

    [1] little fix 2

--- lab_01_02/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_lab_01_02_FILE C:/Users/User/Desktop/Projects/sgn3-cprog-labs-2024-sirenko-grigorij-dmitrievich/lab_01_02/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/lab_01_02.exe)
-set(__QT_DEPLOY_TARGET_lab_01_02_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_lab_01_02_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
