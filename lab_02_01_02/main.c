5b1f883e9140d7d1ded182967318906ea5d15547
SirGrigorya <grisha.sirencko@yandex.ru>
2024-09-28 14:42:58 +0300

    [2] first functions added

--- lab_02_01_02/main.c
+++ lab_02_01_02/main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <math.h>
 
-int main()
-{
-    printf("Hello World!\n");
-    return 0;
+int readCoordinates(float *x, float *y, float *z) {
+    int result = 1;
+    if (scanf("%f%f%f", x, y, z) != 3) {
+        result = 0;
+    }
+    return result;
+}
+
+int findStep(float x, float y, float z) {
+    float step;
+    step = (fabs(x) + fabs(y)) / (z - 1);
+        return step;
+}
+
+int mathFunction(float x) {
+    float res = tan(x) + sqrt(x);
+    return res;
+}
+
+int main() {
+    float x, y, z;
+    if (!readCoordinates(&x, &y, &z)) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
